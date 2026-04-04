8f4fe9ab1ecfd2a5cc413114baf867e0808da615
Mazo667 <maxifava667@gmail.com>
2026-04-04 01:43:01 -0300

    Completo mi portafolio

--- env/frontend-web/src/app/app.routes.ts
+++ env/frontend-web/src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
-import { Home } from './pages/home/home';
 import { IntroComponent } from './pages/optifinanzas/intro/intro';
 import { Terminos } from './pages/optifinanzas/terminos/terminos';
 import { Privacidad } from './pages/optifinanzas/privacidad/privacidad';
+import { HomeComponent } from './pages/home/home';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirige la raíz al home
-    { path: 'home', component: Home },
+    { path: 'home', component: HomeComponent },
     { path: 'optifinanzas', component: IntroComponent },
     { path: 'optifinanzas/terminos-y-condiciones', component: Terminos },
     { path: 'optifinanzas/politica-de-privacidad', component: Privacidad },
--- env/frontend-web/src/app/pages/home/home.ts
+++ env/frontend-web/src/app/pages/home/home.ts
@@ -1,9 +1,31 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule], // <-- Fundamental para usar routerLink
   templateUrl: './home.html',
-  styleUrl: './home.css',
 })
-export class Home {}
+export class HomeComponent {
+
+  // Aquí agregas, editas o eliminas tus proyectos en el futuro.
+  // El HTML se actualizará automáticamente.
+  misProyectos = [
+    {
+      titulo: 'OptiFinanzas',
+      descripcion: 'Gestor financiero inteligente diseñado para ayudarte a optimizar ingresos y aplicar la regla 50/30/20 día a día.',
+      imagen: 'assets/icono_optifinanzas_no_bg.svg', // Icono de tu app
+      etiquetas: ['Android', 'Finanzas', 'Kotlin'],
+      ruta: '/optifinanzas' // La ruta de la intro que ya creamos
+    },
+    {
+      titulo: 'OptiStock',
+      descripcion: 'Sistema avanzado de inventario y control de stock para optimizar la cadena de suministro.',
+      imagen: 'assets/optistock_logo.svg', // Reemplaza con una imagen real luego
+      etiquetas: ['Django', 'Python', 'Web'],
+      ruta: '/optistock' // La ruta que crearás en el futuro
+    },
+  ];
+
+}
