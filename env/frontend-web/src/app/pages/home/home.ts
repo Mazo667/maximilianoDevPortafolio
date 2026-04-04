6db1c96a5af3dc9b34b364dabaa85becb35a1ba6
Mazo667 <maxifava667@gmail.com>
2026-04-04 02:22:18 -0300

    Termino de implementar las politicas de pricacidad y terminos y condiciones

--- env/frontend-web/src/app/pages/home/home.ts
+++ env/frontend-web/src/app/pages/home/home.ts
@@ -4,27 +4,26 @@ import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule], // <-- Fundamental para usar routerLink
+  imports: [RouterModule],
   templateUrl: './home.html',
 })
+
 export class HomeComponent {
 
-  // Aquí agregas, editas o eliminas tus proyectos en el futuro.
-  // El HTML se actualizará automáticamente.
   misProyectos = [
     {
       titulo: 'OptiFinanzas',
       descripcion: 'Gestor financiero inteligente diseñado para ayudarte a optimizar ingresos y aplicar la regla 50/30/20 día a día.',
-      imagen: 'assets/icono_optifinanzas_no_bg.svg', // Icono de tu app
+      imagen: 'assets/icono_optifinanzas_no_bg.svg',
       etiquetas: ['Android', 'Finanzas', 'Kotlin'],
-      ruta: '/optifinanzas' // La ruta de la intro que ya creamos
+      ruta: '/optifinanzas'
     },
     {
       titulo: 'OptiStock',
       descripcion: 'Sistema avanzado de inventario y control de stock para optimizar la cadena de suministro.',
-      imagen: 'assets/optistock_logo.svg', // Reemplaza con una imagen real luego
+      imagen: 'assets/optistock_logo.svg',
       etiquetas: ['Django', 'Python', 'Web'],
-      ruta: '/optistock' // La ruta que crearás en el futuro
+      ruta: '/optistock' //TODO: Crear ruta
     },
   ];
 
